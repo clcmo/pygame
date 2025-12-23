11598b59d31500bd71c2319d0a3bbdfc7d0b4bcc
Camila L. Oliveira <suporte@camilaloliveira.com.br>
2025-12-23 11:40:35 -0300

    Feat: main com os comandos principais do jogo

---
+++ main.py
@@ -0,0 +1,27 @@
+from model import GameModel
+import view
+
+game = GameModel()
+
+def update():
+    game.update()
+
+def on_key_down(key):
+    if game.state == "menu":
+        if key == keys.S: game.state = "playing"
+        elif key == keys.M: game.sound_on = not game.sound_on
+        elif key == keys.E: exit()
+    elif game.state == "playing":
+        if key == keys.RIGHT: game.hero.move(game.hero.speed, 0)
+        if key == keys.LEFT: game.hero.move(-game.hero.speed, 0)
+        if key == keys.UP: game.hero.move(0, -game.hero.speed)
+        if key == keys.DOWN: game.hero.move(0, game.hero.speed)
+    elif game.state in ["game_over", "victory"]:
+        if key == keys.R:  # Restart
+            game.__init__()
+
+def draw():
+    view.draw(game.hero, game.enemies, game.state, game.hero_lives)
+
+import pgzrun
+pgzrun.go()
