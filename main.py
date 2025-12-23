1caf9e842be2639f089e7305442b72371bda2d72
Camila L. Oliveira <suporte@camilaloliveira.com.br>
2025-12-23 11:45:50 -0300

    Fix controller para que receba o input corretamente para os métodos de pausa e ataque no jogo

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ game = GameModel()
 
 def update():
     game.update()
+    game.hero.attacking = False  # ataque dura apenas 1 frame
 
 def on_key_down(key):
     if game.state == "menu":
@@ -16,9 +17,12 @@ def on_key_down(key):
         if key == keys.LEFT: game.hero.move(-game.hero.speed, 0)
         if key == keys.UP: game.hero.move(0, -game.hero.speed)
         if key == keys.DOWN: game.hero.move(0, game.hero.speed)
+        if key == keys.SPACE: game.hero.attack()
+        if key == keys.P: game.state = "paused"
     elif game.state in ["game_over", "victory"]:
-        if key == keys.R:  # Restart
-            game.__init__()
+        if key == keys.R: game.__init__()
+    elif game.state == "paused":
+        if key == keys.P: game.state = "playing"
 
 def draw():
     view.draw(game.hero, game.enemies, game.state, game.hero_lives)
